598619615e0e0e13220f5f8bcccb66864dc67eb9
Nikhil Dash <nikhild@mindfiresolutions.com>
2023-10-10 18:09:03 +0530

    Add migrations,models and associations

---
+++ db/migrate/20231010115944_create_order_items.rb
@@ -0,0 +1,13 @@
+class CreateOrderItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :order_items do |t|
+      t.references :product, null: false, foreign_key: true
+      t.string :product_name
+      t.string :product_description
+      t.decimal :price_per_unit, precision: 10, scale: 2
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
