7ef47339063860584fdb60f38fda85e84590d5d5
Nikhil Dash <nikhild@mindfiresolutions.com>
2023-10-16 13:26:13 +0530

    Add session check routes and add CRUD to articles and comments ,also show articles specific to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
+        @articles = current_user.articles
     end
 
     def new
